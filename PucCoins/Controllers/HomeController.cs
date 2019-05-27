00476a909f370fd00d88c29cd1d8e5efa96295a1
Thiago Lopes <tvplopes@outlook.com>
2019-05-26 23:20:27 -0300

    Add project files.

---
+++ PucCoins/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PucCoins.Models;
+
+namespace PucCoins.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MinhasMoedas()
+        {
+            return View();
+        }
+
+        public IActionResult Transferencias()
+        {
+            return View();
+        }
+
+        public IActionResult Relatorios()
+        {
+            return View();
+        }
+
+        public IActionResult Resgatar()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
